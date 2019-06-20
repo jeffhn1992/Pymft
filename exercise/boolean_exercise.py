1aea5b2e4ae7fdedc3762ee0733121a32320e771
FirstCommitForTest <jhejazynia@gmail.com>
2019-06-20 18:05:11 +0430

    This is the first commit on GIT just for test

---
+++ exercise/boolean_exercise.py
@@ -0,0 +1,5 @@
+num = 13
+
+condition = (20>num>10 or 40>num>30)
+
+print(condition)
--- exercise/list_exercise.py
+++ exercise/list_exercise.py
@@ -5,3 +5,11 @@ ave = sum(list_test)/ len(list_test)
 print(ave)
 mean_list = mean(list_test)
 print(mean_list)
+
+reverse_list = list_test[::-2]
+
+first_list = list_test[:5]
+second_list = list_test[5::-1]
+third_list = first_list + second_list
+print(first_list, second_list)
+print(third_list)
