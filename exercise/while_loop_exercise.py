43dc8bb7b6f790bb89174e28e501f2b36491f0b6
FirstCommitForTest <jhejazynia@gmail.com>
2019-06-21 15:38:57 +0430

    2nd session end

---
+++ exercise/while_loop_exercise.py
@@ -0,0 +1,16 @@
+a = 10
+b = 20
+sum_num = a
+factorial_num = a
+
+while a<=b:
+    a = a + 1
+    sum_num = sum_num + a
+    factorial_num = factorial_num * a
+
+print(sum_num)
+print(factorial_num)
+
+
+
+
